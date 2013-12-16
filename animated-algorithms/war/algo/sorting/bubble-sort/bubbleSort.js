38063819bc2d5f7c0fc416b7e1872ee657dcb745
moazzam.cricket@gmail.com <moazzam.cricket@gmail.com@43d6f08d-b54f-1580-322f-d45199cd3a76>
2013-12-16 10:12:37 +0000

    Initial Checkin

---
+++ animated-algorithms/war/algo/sorting/bubble-sort/bubbleSort.js
@@ -0,0 +1,69 @@
+﻿function clearContents() {
+    var mainDiv = document.getElementById('content');
+    mainDiv.innerHTML = "";
+}
+
+var inputArray;
+
+function parseArray() {
+    inputArray = [];
+    var input = document.getElementById('numberInput');
+    var items = input.value.split(',');
+    for (var i in items)
+        inputArray.push(items[i]);
+}
+
+function showArrayContents() {
+
+    parseArray();
+    generateArrayContents();
+}
+
+function generateArrayContents(number) {
+    var mainContent = document.getElementById('content');
+
+    var inputArrayHtml = "<div class='mainArray' id='ArrayContents" +number+"'>";
+
+    for (var i in inputArray) {
+        inputArrayHtml += "<div class='num'>" + inputArray[i] + "</div>";
+        }
+   
+    mainContent.innerHTML += inputArrayHtml + "</div>";
+}
+
+function generateSwappedArray(number, start, end) {
+    var mainContent = document.getElementById('content');
+
+    var inputArrayHtml = "<div class='mainArray' id='ArrayContents" + number + "'>";;
+
+    for (var i in inputArray) 
+    {
+        if (parseInt(i) === start || parseInt(i) === end) {
+            inputArrayHtml += "<div class='swapped'>" + inputArray[i] + "</div>";
+        }
+        else
+        {
+            inputArrayHtml += "<div class='num'>" + inputArray[i] + "</div>";
+        }
+    }
+    mainContent.innerHTML += inputArrayHtml + "</div>";
+}
+
+function bubbleSort() {
+
+    for (var i = 0; i < inputArray.length; i++) {
+        for (var j = 0; j < inputArray.length - i - 1; j++) {
+            if (parseInt(inputArray[j + 1]) < parseInt(inputArray[j])) {
+
+                generateSwappedArray(j, j, j + 1);
+                var temp = inputArray[j];
+                inputArray[j] = inputArray[j + 1];
+                inputArray[j + 1] = temp;
+
+               // generateSwappedArray(j, j, j + 1);
+            }
+        }
+       
+    }
+    generateArrayContents(19090);
+}
