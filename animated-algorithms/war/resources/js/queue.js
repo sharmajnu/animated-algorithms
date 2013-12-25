8f7df6f8eac9ca0f14ba9a5306d7353a61dbef13
sharma.jnu@gmail.com <sharma.jnu@gmail.com@43d6f08d-b54f-1580-322f-d45199cd3a76>
2013-12-25 17:29:13 +0000

    binary-search

---
+++ animated-algorithms/war/resources/js/queue.js
@@ -0,0 +1,19 @@
+﻿function Queue() {
+    this.array = [];
+}
+
+
+Queue.prototype.enqueue = function (element) {
+    this.array.push(element);
+};
+
+Queue.prototype.dequeue = function () {
+    var element = this.array[0];
+    return this.array.splice(0, 1)[0];
+};
+
+Queue.prototype.IsEmpty = function () {
+    if (this.array.length === 0)
+        return true;
+    return false;
+};
